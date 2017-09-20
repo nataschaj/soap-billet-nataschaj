37490511c420ad270654c788a6218e60d5de3cd6
natsachaj <natascha_jakobsen@hotmail.com>
2017-09-20 10:55:29 +0200

    Test tilføjet. Test af bilens pris -> Grøn. Test af MC pris -> Grøn. Test af brobizz med bil pris -> Grøn.

---
+++ WcfBilletTests/Service1Tests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfBillet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Billet;
+
+namespace WcfBillet.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        [TestMethod()]
+        public void GetBilPrisTest()
+        {
+            var bil = new Bil();
+            int pris = bil.Pris();
+            Assert.AreEqual(240, pris);
+        }
+
+        [TestMethod()]
+        public void GetMCPrisTest()
+        {
+            var mc = new MC();
+            int pris = mc.Pris();
+            Assert.AreEqual(125, pris);
+        }
+
+        [TestMethod()]
+        public void GetBilBrobizzTest()
+        {
+            var bil = new Bil();
+            bil.Brobizz = true;
+            Assert.AreEqual(228, bil.Pris());
+        }
+
+       
+
+        //[TestMethod()]
+        //public void GetDataTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void GetDataUsingDataContractTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+
+    }
+}
